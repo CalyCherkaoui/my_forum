505dc2fddc65ba3aa81069cbcc853c1fe114d04b
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-09-27 16:01:49 -0500

    Adding devise controllers for authors

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :authors
   root to: 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/about'
+
+  devise_for :authors, controllers: {
+    sessions: 'authors/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
