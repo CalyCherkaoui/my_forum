e45c90ce75626f56c22ec5af40ad9307a8021934
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-09-27 15:53:01 -0500

    Create Author model with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  devise_for :authors
+  root to: 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
