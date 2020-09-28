e212feb7dca6df3ba80b0e147a47d01a7927307f
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-09-28 14:52:47 -0500

    Adding authentication forms html layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,6 @@ module ApplicationHelper
 
   def authentication_view_helper (author_signed, current_author)
     if author_signed
-
       link1 = link_to( current_author.name, author_path(current_author), class: "nav-list__link")
       msg_html1 = content_tag(:li, link1.html_safe, class: 'nav-list__item')
 
@@ -14,7 +13,6 @@ module ApplicationHelper
       msg_html_1 = content_tag(:li, link1.html_safe , class: 'nav-list__item')
 
       link2 = link_to("Sign-up", new_author_registration_path, class: "nav-list__link")
-
       msg_html_2 = content_tag(:li, link2.html_safe, class: 'nav-list__item')
 
     end
