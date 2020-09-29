d4119e06f88231640e904304566ff6da76426f77
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-09-29 13:21:31 -0500

    Adding devise controllers and views for author

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,9 @@
+class AuthorsController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :controllers
   root to: 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/about'
