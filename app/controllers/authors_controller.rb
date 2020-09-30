5fed26962c4e1544a02d3994934f4fb7dcfda99d
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-09-29 20:45:59 -0500

    Adding authors controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,9 +1,17 @@
 class AuthorsController < ApplicationController
-  def index
+  before_action :authors_signed_in?, only: %i[edit destroy]
 
+  def index
+    @authors = Authors.all
   end
 
   def show
+    @author = Author.find(params[:id])
+  end
 
+  private
+
+  def user_params
+    params.require(:authors).permit(:id, :name, :username, :email, :password, :password_confirmation)
   end
 end
