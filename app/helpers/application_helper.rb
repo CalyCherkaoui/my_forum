1bff94e857eaf38324715c377578092bc6a531a5
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-02 14:12:29 -0500

    helper for authentication view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,33 @@
 module ApplicationHelper
 
-  def authentication_view_helper (author_signed, current_author)
-    if author_signed
-      link1 = link_to( current_author.name, author_path(current_author), class: "nav-list__link")
-      msg_html1 = content_tag(:li, link1.html_safe, class: 'nav-list__item')
+  def authentication_view_helper
+    link1 = ''
+    link2 = ''
+    msg_html_1 = ''
+    msg_html_2 = ''
 
-      link2 = link_to( "log-out", destroy_author_session_path, class: "nav-list__link" )
-      msg_html2 = content_tag(:li, link2.html_safe, class: 'nav-list__item')
+    if author_signed_in?
+      link1 = link_to(author_path(current_author.id), class: "nav-list__link") do 
+       current_author.email
+      end
+
+      msg_html1 = content_tag(:li, link1, class: 'nav-list__item')
+
+      link2 = link_to( "log-out", destroy_author_session_path, class: "nav-list__link")
+      msg_html2 = content_tag(:li, link2, class: 'nav-list__item')
 
     else
       link1 = link_to("Log-in", new_author_session_path, class: "nav-list__link")
-      msg_html_1 = content_tag(:li, link1.html_safe , class: 'nav-list__item')
+      msg_html_1 = content_tag(:li, link1, class: 'nav-list__item')
 
       link2 = link_to("Sign-up", new_author_registration_path, class: "nav-list__link")
-      msg_html_2 = content_tag(:li, link2.html_safe, class: 'nav-list__item')
+      msg_html_2 = content_tag(:li, link2, class: 'nav-list__item')
 
     end
 
-    return msg_html_1.concat(msg_html_2).html_safe
+    final_html = msg_html_1 + msg_html_2
+
+    return final_html.html_safe
 
   end
 
