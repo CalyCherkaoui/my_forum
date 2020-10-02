40741354b12f3cca15b3174b1ddada65098fb8a7
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-02 16:21:53 -0500

    Alert closing panel with js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,9 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+$(document).on('turbolinks:load',function(){
+  setTimeout(function() {
+   $('#flash-panel').slideUp('500');
+  }, 5000);
+});
