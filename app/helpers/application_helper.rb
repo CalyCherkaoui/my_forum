6fe827ccbb44f0811cda8d4e5a6a40f772212aae
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-02 16:21:28 -0500

    styling alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,4 +31,24 @@ module ApplicationHelper
 
   end
 
+  def flash_class_helper(type)
+    case type.to_sym
+    when :notice then "success"
+    when :success then "success"
+    when :error then "danger"
+    when :alert then "danger"
+    when :warning then "warning"
+    end
+  end
+
+  def flash_prefix_helper(type)
+    case type.to_sym
+    when :notice then "Notice!"
+    when :success then "Success!"
+    when :error then "Error!"
+    when :alert then "Alert!"
+    when :warning then "Warning!"
+    end 
+  end
+
 end
