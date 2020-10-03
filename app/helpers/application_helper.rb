d3850c04c67ae7aa97021b3665192a7a830b7ad5
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-03 18:47:14 -0500

    test for flash naming html div classes helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,36 +1,5 @@
 module ApplicationHelper
 
-  def authentication_view_helper
-    link1 = ''
-    link2 = ''
-    msg_html_1 = ''
-    msg_html_2 = ''
-
-    if author_signed_in?
-      link1 = link_to(author_path(current_author.id), class: "nav-list__link") do 
-       current_author.email
-      end
-
-      msg_html1 = content_tag(:li, link1, class: 'nav-list__item')
-
-      link2 = link_to( "log-out", destroy_author_session_path, class: "nav-list__link")
-      msg_html2 = content_tag(:li, link2, class: 'nav-list__item')
-
-    else
-      link1 = link_to("Log-in", new_author_session_path, class: "nav-list__link")
-      msg_html_1 = content_tag(:li, link1, class: 'nav-list__item')
-
-      link2 = link_to("Sign-up", new_author_registration_path, class: "nav-list__link")
-      msg_html_2 = content_tag(:li, link2, class: 'nav-list__item')
-
-    end
-
-    final_html = msg_html_1 + msg_html_2
-
-    return final_html.html_safe
-
-  end
-
   def flash_class_helper(type)
     case type.to_sym
     when :notice then "success"
@@ -50,5 +19,36 @@ module ApplicationHelper
     when :warning then "Warning!"
     end 
   end
+
+  # def authentication_view_helper(signed_in, current_author)
+  #   link1 = ''
+  #   link2 = ''
+  #   msg_html_1 = ''
+  #   msg_html_2 = ''
+
+  #   if signed_in
+  #     link1 = link_to(author_path(current_author.id), class: "nav-list__link") do 
+  #      current_author.email
+  #     end
+
+  #     msg_html1 = content_tag(:li, link1, class: 'nav-list__item')
+
+  #     link2 = link_to( "log-out", destroy_author_session_path, class: "nav-list__link")
+  #     msg_html2 = content_tag(:li, link2, class: 'nav-list__item')
+
+  #   else
+  #     link1 = link_to("Log-in", new_author_session_path, class: "nav-list__link")
+  #     msg_html_1 = content_tag(:li, link1, class: 'nav-list__item')
+
+  #     link2 = link_to("Sign-up", new_author_registration_path, class: "nav-list__link")
+  #     msg_html_2 = content_tag(:li, link2, class: 'nav-list__item')
+
+  #   end
+
+  #   final_html = msg_html_1 + msg_html_2
+
+  #   return final_html.html_safe
+
+  # end
 
 end
