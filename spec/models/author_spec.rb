1777b2b0cee0c89ae59269ee41eb24dd63e75281
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-03 17:27:12 -0500

    testing author model

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'email validations' do
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email)}
+    it { should_not allow_value("not an email").for(:email) }
+    it { should allow_value("user@exeple.com").for(:email) }
+  end
 end
