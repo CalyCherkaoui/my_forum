c36872292a14e86017a7bc971f0f940f177bb931
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-07 12:36:32 -0500

    adding author profile editor View & controllers

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
 
   devise_for :authors, controllers: {
     sessions: 'authors/sessions',
-    registrations: 'authors/registrations'
+    registrations: 'authors/registrations',
+    passwords: 'authors/passwords'
   }
 
-  resources :authors, only: %i[index show destroy]
+  resources :authors, only: %i[index show edit destroy]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
