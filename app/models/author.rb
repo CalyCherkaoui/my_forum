06116067128087d5f06190006af4efaa5f8602b3
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-08 16:18:50 -0500

    update author

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,10 +1,17 @@
 class Author < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  after_create :set_profile
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :email, uniqueness: true
   validates :username, uniqueness: true, presence: true
-  has_one :profile
-  accepts_nested_attributes_for :profile, reject_if: :all_blank, allow_destroy: true, update_only: true
+  has_one :profile, dependent: :destroy
+  accepts_nested_attributes_for :profile, allow_destroy: true, update_only: true
+
+  protected
+
+  def set_profile
+    Profile.create!({ :author_id => self.id})
+  end
 end
