b752c45c0684f4109b8c1b2b483daccfee7d0db4
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-08 16:17:51 -0500

    Updating author

--- app/controllers/authors/sessions_controller.rb
+++ app/controllers/authors/sessions_controller.rb
@@ -22,6 +22,6 @@ class Authors::SessionsController < Devise::SessionsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_in_params
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :username])
+    devise_parameter_sanitizer.permit(:sign_in)
   end
 end
