8ddd02ef819eafb019868eebf1b88887617ae57c
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-10 20:39:53 -0500

    Updating profile model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,5 @@
 class Profile < ApplicationRecord
-  belongs_to :author, optional: true
+  belongs_to :author
 
   # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   # validates :email, :format => VALID_EMAIL_REGEX
