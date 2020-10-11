7bc005890e81f2eaf4cff823de0dd715bb77fd08
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-11 17:21:54 -0500

    update author's show view

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,6 @@
 class Profile < ApplicationRecord
   after_create :add_default_image
+  # after_update :size_photo
   belongs_to :author
   has_one_attached :image
   # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
@@ -17,16 +18,19 @@ class Profile < ApplicationRecord
     output
   end
 
-  def sized_photo
-    return self.image.variant(resize: '300x300').processed
-  end
+
 
 
   private
 
+  def size_photo
+    self.image.variant(resize: '300x300!').processed
+  end
+
   def add_default_image
       self.image.attach(io: File.open(Rails.root.join("app", "assets", "images", "default_author_profile.jpg")), filename: 'default_author_profile.jpg', content_type: 'image/jpg')
       # self.image.attach(asset_path("default_author_profile.jpg"))
+      self.image.variant(resize: '100x100!').processed
   end
 
 end
